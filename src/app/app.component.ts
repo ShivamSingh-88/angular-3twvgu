4c6fc517193be2dde18abcd753cfc212ccb5bbef
ShivamSingh-88 <115132888+ShivamSingh-88@users.noreply.github.com>
2022-11-14 15:53:08 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,82 +8,77 @@ import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
 })
 export class AppComponent {
   form = new FormGroup({});
-  model:any ={
-    id:[''],
-    firstName:[''],
-    lastName:[''],
-    gender:[''],
-    email:[''],
-    department:[''],
-  }
-  fields: FormlyFieldConfig[]=[
+  model: any = {
+    id: [''],
+    firstName: [''],
+    lastName: [''],
+    gender: [''],
+    email: [''],
+    department: [''],
+  };
+  fields: FormlyFieldConfig[] = [
     {
-      key:'id',
-      type:'input',
-      templateOptions:{
+      key: 'id',
+      type: 'input',
+      templateOptions: {
         label: 'Id',
-        type: 'number',
-        required:true
-      }
-
+        type: 'string',
+        required: true,
+      },
+      validators: {
+        validation: ['id'],
+      },
     },
     {
-      key:'firstName',
-      type:'input',
-      templateOptions:{
+      key: 'firstName',
+      type: 'input',
+      templateOptions: {
         label: 'FirstName',
         type: 'string',
-        required:true
-      }
-
+        required: true,
+      },
     },
     {
-      key:'lastName',
-      type:'input',
-      templateOptions:{
+      key: 'lastName',
+      type: 'input',
+      templateOptions: {
         label: 'LastName',
         type: 'string',
-        required:true
-      }
-
+        required: true,
+      },
     },
     {
-      key:'gender',
-      type:'input',
-      templateOptions:{
+      key: 'gender',
+      type: 'input',
+      templateOptions: {
         label: 'Gender',
         type: 'string',
-        required:true
-      }
-
+        required: true,
+      },
     },
     {
-      key:'email',
-      type:'input',
-      templateOptions:{
+      key: 'email',
+      type: 'input',
+      templateOptions: {
         label: 'Email',
         type: 'string',
-        required:true
-      }
-
+        required: true,
+      },
     },
     {
-      key:'department',
-      type:'input',
-      templateOptions:{
+      key: 'department',
+      type: 'input',
+      templateOptions: {
         label: 'Department',
         type: 'string',
-        required:true
-      }
-
-    }
-
+        required: true,
+      },
+    },
   ];
   onSubmit(model: any) {
     console.log(model);
   }
 }
-
 
 /**  Copyright 2021 Formly. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  ReactiveFormsModule,
+  ValidationErrors,
+} from '@angular/forms';
 import { FormlyModule, FormlyFieldConfig } from '@ngx-formly/core';
 import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
 
@@ -9,17 +13,37 @@ import { AppComponent } from './app.component';
 export function RequiredValidatorMessage(error: any, field: FormlyFieldConfig) {
   return `"This required field was validated after ${field.formControl.updateOn}"`;
 }
-
+export function IdValidatorMessage(error: any, field: FormlyFieldConfig) {
+  return `"${field.formControl.value} is not vallid ID`;
+}
+const ALPHA_NUMERIC_VALIDATION_ERROR = {
+  alphaNumericError: 'only alpha numeric values are allowed',
+};
+export function IdValidator(control: AbstractControl): ValidationErrors | null {
+  return /^[a-zA-Z0-9_]$/.test(control.value)
+    ? null
+    : ALPHA_NUMERIC_VALIDATION_ERROR;
+}
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     FormlyBootstrapModule,
     FormlyModule.forRoot({
+      validators: [
+        {
+          name: 'id',
+          validation: IdValidator,
+        },
+      ],
       validationMessages: [
         {
+          name: 'id',
+          message: IdValidatorMessage,
+        },
+        {
           name: 'required',
-          message: RequiredValidatorMessage,
+          message: 'Field is required',
         },
       ],
     }),
@@ -28,7 +52,6 @@ export function RequiredValidatorMessage(error: any, field: FormlyFieldConfig) {
   declarations: [AppComponent],
 })
 export class AppModule {}
-
 
 /**  Copyright 2021 Formly. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
