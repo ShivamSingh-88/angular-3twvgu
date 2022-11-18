e0819a0065deda4f721c0baeb2dae565932015e3
ShivamSingh-88 <115132888+ShivamSingh-88@users.noreply.github.com>
2022-11-18 15:16:34 +0530

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'formly-app-example',
@@ -15,7 +16,11 @@ export class AppComponent {
     gender: [''],
     email: [''],
     department: [''],
+    stateId:[''],
+    placeId:['']
   };
+
+  // constructor(private dataService: DataService) { }
   fields: FormlyFieldConfig[] = [
     {
       key: 'id',
@@ -72,8 +77,136 @@ export class AppComponent {
         label: 'Department',
         type: 'string',
         required: true,
-      },
+      }, 
     },
+    {
+      key: 'stateId',
+      type: 'select',
+      templateOptions: {
+        label: 'State',
+        // options:this.dataService.getState()
+        options:[
+          {
+            value:1,
+            label:"Uttar Pradesh"
+           },
+           {
+            value:2,
+            label:"Andhra Pradesh"
+           }
+           ,
+           {
+            value:3,
+            label:"Madhya Pradesh"
+           }
+           
+      
+
+        ],
+        
+        type: 'string',
+        required: true,
+      }
+    },
+      {
+        key: 'placeId',
+        type: 'select',
+        templateOptions: {
+          label: 'Place',
+          options:[
+            {
+              value: null,
+              label:"----",
+              stateId:null
+            },
+            {
+             value:1,
+             label:"Noida",
+             stateId:1
+            },
+            {
+              value:2,
+              label:"Lucknow",
+              stateId:1
+             },
+             {
+              value:3,
+              label:"Agra",
+              stateId:1
+             },
+             {
+              value:4,
+              label:"Varanasi",
+              stateId:1
+             },
+             {
+              value:5,
+              label:"Prayagraj",
+              stateId:1
+             },
+        
+        
+             {
+              value:1,
+              label:"Tirupati",
+              stateId:2
+             },
+             {
+               value:2,
+               label:"Vijaywada",
+               stateId:2
+              },
+              {
+               value:3,
+               label:"Guntoor",
+               stateId:2
+              },
+              {
+               value:4,
+               label:"Nellore",
+               stateId:2
+              },
+              {
+               value:5,
+               label:"Kadapa",
+               stateId:2
+              }
+            ,
+        
+        
+            {
+              value:1,
+              label:"Indore",
+              stateId:3
+             },
+             {
+               value:2,
+               label:"Bhopal",
+               stateId:3
+              },
+              {
+               value:3,
+               label:"Mhow",
+               stateId:3
+              },
+              {
+               value:4,
+               label:"Ujjain",
+               stateId:3
+              },
+              {
+               value:5,
+               label:"Gwalior",
+               stateId:3
+              }
+            
+    
+          ],
+          
+          type: 'string',
+          required: true,
+        }
+      }
   ];
   onSubmit(model: any) {
     console.log(model);
